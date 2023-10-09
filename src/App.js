21f72f1422a4f28ed08256b8012ae2c31cdbd80b
harshitgupta2643 <harshitgupta2643@gmail.com>
2023-10-09 13:17:12 +0530

    doughnut charts and main form done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import {Grid} from '@material-ui/core';
+import Main from './components/Main/Main';
+import Details from './components/Details/Details'
+import useStyles from './style';
 const App = () => {
+    const classes = useStyles();
     return (
-        <div>App</div>
+        <Grid className={classes.grid} container spacing ={0} alignItems="center" justify="center" style={{height : '100vh'}}>
+            <Grid item xs={12} sm={4}>
+                <Details title="Income"/>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+                <Main />
+            </Grid><Grid item xs={12} sm={4}>
+                <Details title ="Expense"/>
+            </Grid>
+        </Grid>
     )
 }
 
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
+import { ArcElement, Chart } from 'chart.js';
+import useStyles from './style';
+import useTransactions from '../../useTransaction';
+Chart.register(ArcElement);
+const DetailsCard = ({ title, subheader }) => {
+    const { total, chartData } = useTransactions(title);
+    const classes = useStyles();
+
+    return (
+        <Card className={title === 'Income' ? classes.income : classes.expense}>
+            <CardHeader title={title} subheader={subheader} />
+            <CardContent>
+                <Typography variant="h5">${total}</Typography>
+                <Doughnut data={chartData} />
+            </CardContent>
+        </Card>
+    );
+};
+
+export default DetailsCard;
